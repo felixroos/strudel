445f24a639fef9c16991925781052add7a5ddc94
Felix Roos <flix91@gmail.com>
2023-11-09 17:11:40 +0100

    use dough-samples

--- iclc2023/slides/src/prebake.mjs
+++ iclc2023/slides/src/prebake.mjs
@@ -2,21 +2,20 @@ import { registerSynthSounds, samples } from '@strudel.cycles/webaudio';
 import { registerSoundfonts } from '@strudel.cycles/soundfonts';
 
 export async function prebake() {
-  // https://archive.org/details/SalamanderGrandPianoV3
-  // License: CC-by http://creativecommons.org/licenses/by/3.0/ Author: Alexander Holm
+  const ds = 'https://raw.githubusercontent.com/felixroos/dough-samples/main/';
   await Promise.all([
     registerSynthSounds(),
-    registerSoundfonts(`./samples/webaudiofontdata/sound/`),
-    /* samples(`./piano.json`, `./piano/`, { prebake: true }),
-    samples(`./vcsl.json`, 'github:sgossner/VCSL/master/', { prebake: true }),
-    */
-    //samples(`./tidal-drum-machines.json`, 'github:ritchse/tidal-drum-machines/main/machines/', {
+    registerSoundfonts(),
+    samples(`${ds}/tidal-drum-machines.json`),
+    samples(`${ds}/piano.json`),
+    samples(`${ds}/Dirt-Samples.json`),
+    samples(`${ds}/EmuSP12.json`),
+    samples(`${ds}/vcsl.json`),
+    /* registerSoundfonts(`./samples/webaudiofontdata/sound/`),
     samples(`./tidal-drum-machines.json`, './samples/tidal-drum-machines/machines/', {
       prebake: true,
       tag: 'drum-machines',
-    }) /*
-    samples(`./EmuSP12.json`, `./EmuSP12/`, { prebake: true, tag: 'drum-machines' }), */,
-    // samples('github:tidalcycles/Dirt-Samples/master'),
-    samples('./Dirt-Samples.json', './samples/Dirt-Samples/'),
+    }) 
+    samples('./Dirt-Samples.json', './samples/Dirt-Samples/'), */
   ]);
 }
